7de778e26d2c6aa41e9292fdcf15666e0794bf6d
John <jpkelly312@gmail.com>
2020-11-17 18:16:01 -0500

    FrontEnd basics done, CSS needed and some refactoring

--- client/src/components/ReviewAverage.jsx
+++ client/src/components/ReviewAverage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import ReviewStars from './ReviewStars.jsx'
+import ReviewBar from './ReviewBar.jsx'
+
 
 class ReviewAverage extends React.Component {
   constructor(props){
@@ -66,15 +69,14 @@ class ReviewAverage extends React.Component {
     return(
       <div>
         <div > <p>{this.getOverall(reviews)}</p></div>
-        <div > <p>({reviews.length})</p></div>
+        <div > <p> <ReviewStars value={this.getOverall(reviews)}/> ({reviews.length})</p></div>
         <div > Average customer ratings</div>
         <div><br></br></div>
-
-        <div ><p>Ease of assembly/installation </p></div>
-        <div ><p>Value for money {this.getValue(reviews)}</p></div>
-        <div ><p>Product quality {this.getQuality(reviews)}</p></div>
-        <div ><p>Appearance {this.getAppearance(reviews)}</p></div>
-        <div ><p>Works as expected {this.getExpected(reviews)}</p></div>
+        <div ><p>Ease of assembly/installation <ReviewBar completed={this.getAssembly(reviews)}/>{this.getAssembly(reviews)}</p></div>
+        <div ><p>Value for money <ReviewBar completed={this.getValue(reviews)}/>{this.getValue(reviews)}</p></div>
+        <div ><p>Product quality <ReviewBar completed={this.getQuality(reviews)}/>{this.getQuality(reviews)}</p></div>
+        <div ><p>Appearance <ReviewBar completed={this.getAppearance(reviews)}/>{this.getAppearance(reviews)}</p></div>
+        <div ><p>Works as expected <ReviewBar completed={this.getExpected(reviews)}/>{this.getExpected(reviews)}</p></div>
         <br></br>
     </div>
     )
--- client/src/components/reviewItem.jsx
+++ client/src/components/reviewItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReviewStars from './ReviewStars.jsx'
+import ReviewBar from './ReviewBar.jsx'
 
 var showRecommend = (recommend) => {
   if (recommend === 1) {
@@ -10,17 +12,17 @@ var showRecommend = (recommend) => {
 
 const ReviewItem = (props) => (
     <div>
-      <div > {props.review.author} {props.review.date.slice(0,10)}</div>
+      <div > <ReviewStars value={props.review.overall_rating}/> {props.review.author} {props.review.date.slice(0,10)}</div>
       <div > {props.review.title}</div>
       <div > {props.review.text}</div>
       <div > <p>{showRecommend(props.review.recommended)}</p></div>
       <div ><br></br></div>
 
-      <div >Ease of assembly/installation {props.review.ease_of_assembly_rating}</div>
-      <div >Value for money {props.review.value_rating}</div>
-      <div >Product quality {props.review.quality_rating}</div>
-      <div >Appearance {props.review.appearance_rating}</div>
-      <div >Works as expected {props.review.works_as_expected_rating}</div>
+      <div >Ease of assembly/installation <ReviewBar completed={props.review.ease_of_assembly_rating}/>{props.review.ease_of_assembly_rating}</div>
+      <div >Value for money <ReviewBar completed={props.review.value_rating}/>{props.review.value_rating}</div>
+      <div >Product quality <ReviewBar completed={props.review.quality_rating}/>{props.review.quality_rating}</div>
+      <div >Appearance <ReviewBar completed={props.review.appearance_rating}/>{props.review.appearance_rating}</div>
+      <div >Works as expected <ReviewBar completed={props.review.works_as_expected_rating}/>{props.review.works_as_expected_rating}</div>
       <br></br>
     </div>
 
