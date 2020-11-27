038b22f5c64d07205ffa48c172da072e31a4e34b
John <jpkelly312@gmail.com>
2020-11-27 14:14:30 -0500

    Fix webapack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,13 @@ module.exports = {
       {
         test: /.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env",
+            "@babel/preset-react"]
+          }
+        }
       },
       {
         test: /\.css$/i,
